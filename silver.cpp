f8e8012314fd356d8a394aa83886c4910b7a2131
Justin Auger <justinauger@gmail.com>
2019-05-20 12:17:18 +0100

    Use y,x ordering everywhere except input/output

--- silver.cpp
+++ silver.cpp
@@ -13,9 +13,9 @@ const vector<pair<int,int>> d4 = {{0,1},{0,-1},{1,0},{-1,0}};
 struct Building {
     int owner;
     int buildingType;
-    int x;
     int y;
-    Building(int owner, int buildingType, int x, int y):owner(owner),buildingType(buildingType),x(x),y(y){}
+    int x;
+    Building(int owner, int buildingType, int y, int x):owner(owner),buildingType(buildingType),y(y),x(x){}
     static int const HQ = 0;
     static int const MINE = 1;
     static int const TOWER = 2;
@@ -24,9 +24,9 @@ struct Building {
 struct Unit {
     int unitId;
     int level;
-    int x;
     int y;
-    Unit(int unitId, int level, int x, int y):unitId(unitId),level(level),x(x),y(y){}
+    int x;
+    Unit(int unitId, int level, int y, int x):unitId(unitId),level(level),y(y),x(x){}
 };
 
 struct Player {
@@ -35,20 +35,20 @@ struct Player {
     static int const NEUTRAL = 2;
 };
 
-vector<vector<int>> calc_dists(vector<vector<bool>> &mask, int tar_x, int tar_y){
+vector<vector<int>> calc_dists(vector<vector<bool>> &mask, int tar_y, int tar_x){
     vector<vector<int>> dist(14, vector<int>(14,INT_MAX));
     queue<tuple<int,int,int>> q;
     dist[tar_y][tar_x] = 0;
     q.push({tar_x, tar_y, 0});
 
     while(!q.empty()){
-        auto [x, y, d] = q.front(); q.pop();
-        for(auto [dx, dy] : d4){
-            int x1 = x + dx;
+        auto [y, x, d] = q.front(); q.pop();
+        for(auto [dy, dx] : d4){
             int y1 = y + dy;
+            int x1 = x + dx;
             if(mask[y1][x1] && dist[y1][x1] == INT_MAX){
                 dist[y1][x1] = d + 1;
-                q.push({x1, y1, d+1});
+                q.push({y1, x1, d+1});
             }
         }
     }
@@ -62,7 +62,6 @@ int main()
     for (int i = 0; i < numberMineSpots; i++) {
         int x, y;
         cin >> x >> y; cin.ignore();
-        cerr << "MINE " << x << "," << y << endl;
     }
 
     for(int tick=1;;tick++) {
@@ -87,7 +86,7 @@ int main()
             }
         }
 
-        int enemy_hq_x, enemy_hq_y;
+        int enemy_hq_y, enemy_hq_x;
         int buildingCount;
         cin >> buildingCount; cin.ignore();
         for (int i = 0; i < buildingCount; i++) {
@@ -95,7 +94,7 @@ int main()
             cin >> owner >> buildingType >> x >> y; cin.ignore();
             x++; y++;
             if(owner == Player::ENEMY && buildingType == Building::HQ) {
-                enemy_hq_x = x; enemy_hq_y = y;
+                enemy_hq_y = y; enemy_hq_x = x;
             }
         }
 
@@ -104,16 +103,16 @@ int main()
         for (int i = 0; i < unitCount; i++) {
             int owner, unitId, level, x, y;
             cin >> owner >> unitId >> level >> x >> y; cin.ignore();
-            x++; y++;
+            y++; x++;
             if (owner == Player::SELF)
-                units.emplace_back(unitId, level, x, y);
+                units.emplace_back(unitId, level, y, x);
             else
-                enemy_units.emplace_back(unitId, level, x, y);
+                enemy_units.emplace_back(unitId, level, y, x);
             tier[y][x] = level;
         }
 
         // distance to enemy hq
-        vector<vector<int>> dist = calc_dists(mask, enemy_hq_x, enemy_hq_y);
+        vector<vector<int>> dist = calc_dists(mask, enemy_hq_y, enemy_hq_x);
 
         string t="";
         // for each unit, move to random unoccupied adjancent space
@@ -121,9 +120,9 @@ int main()
         for(auto u : units){
             vector<pair<int,int>> move_opt;
             int best_pri = INT_MAX;
-            for(auto [dx, dy] : d4){
-                int x1 = u.x + dx;
+            for(auto [dy, dx] : d4){
                 int y1 = u.y + dy;
+                int x1 = u.x + dx;
                 if(!mask[y1][x1]) continue;
                 if(tier[y1][x1] > 0) continue; // can't get through unit (yet)
 
@@ -135,11 +134,11 @@ int main()
                     move_opt.clear();
                 }
                 if(pri == best_pri)
-                    move_opt.push_back({x1,y1});
+                    move_opt.push_back({y1,x1});
                 
             }
             if(move_opt.size() != 0){
-                auto [x1, y1] = move_opt[rand()%move_opt.size()];
+                auto [y1, x1] = move_opt[rand()%move_opt.size()];
                 t += "MOVE " + to_string(u.unitId) + " " + to_string(x1-1) + " " + to_string(y1-1) + ";";
                 owner[y1][x1] = Player::SELF;
             }
@@ -151,9 +150,9 @@ int main()
             for(int i=0;i<14;i++){      // y dir
                 for(int j=0;j<14;j++){  // x dir
                     if(owner[i][j] != Player::SELF) continue;
-                    for(auto [dx, dy] : d4){
-                        int x1 = j + dx;
+                    for(auto [dy, dx] : d4){
                         int y1 = i + dy;
+                        int x1 = j + dx;
                         if(!mask[y1][x1] || owner[y1][x1] == Player::SELF) continue;
                         if(tier[y1][x1] > 0) continue; // can't get through unit (yet)
 
@@ -163,13 +162,13 @@ int main()
                             new_unit_opt.clear();
                         }
                         if(pri == best_pri)
-                            new_unit_opt.push_back({x1,y1});                        
+                            new_unit_opt.push_back({y1,x1});
                     }
                 }
             }
             if(new_unit_opt.size() == 0)
                 break;
-            auto [x1, y1] = new_unit_opt[rand()%new_unit_opt.size()];
+            auto [y1, x1] = new_unit_opt[rand()%new_unit_opt.size()];
             t += "TRAIN 1 " + to_string(x1-1) + " " + to_string(y1-1) + ";";
             owner[y1][x1] = Player::SELF;
             gold -= 10;
